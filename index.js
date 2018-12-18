f042a049c9d41c896407dbc72f149a712dcadf9d
Skylar Salerno <skylar.salerno@gmail.com>
2018-12-18 11:39:41 -0500

    Done.

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 
 function BoardMember(name,homestate,training){
   this.name = name
-  this.homeState = homeState
+  this.homeState = homestate
   this.training = training
   this.veto = () => "No, I must disagree"
   this.approve = () => "You can do that!"
-  this.doCharity =() =>"I like to help people."
+  this.doCharity = () =>"I like to help people."
   this.releasePressStatement=function(){
     return "You will see great things from Scuber."
   }
   this.sayHi= function(){
-    return `Hi, my name is ${this.name}. I am from ${this.homestate}, and I was trained in ${this.training}.`
+    return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
   }
 }
